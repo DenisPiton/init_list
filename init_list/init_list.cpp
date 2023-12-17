ebd8a61d68989c3b4a55028762b6061858a07398
Denis <denfill008@mail.ru>
2023-12-17 13:52:15 +0300

    Добавьте файлы проекта.

---
+++ init_list/init_list.cpp
@@ -0,0 +1,40 @@
+﻿
+#include <iostream>
+#include <initializer_list>
+using namespace std;
+
+
+class Polka {
+    string* books;
+    size_t capacity;
+public:
+    Polka(const initializer_list<string>& list) : capacity{ list.size() } {
+        books = new string[list.size()];
+        int i = 0;
+        for (auto book : list)
+        {
+            books[i] = book;
+            i++;
+        }
+    }
+    void print() {
+        for (size_t i = 0; i < capacity; i++)
+        {
+            cout << books[i]<<" ";
+
+        }
+    }
+    string operator[](int i) {
+        return books[i];
+    }
+
+};
+
+
+int main()
+{
+    Polka b{"Tolstoy","Pushkin","Lermontov"};
+    cout << b[1]<<endl;
+    b.print();
+}
+
